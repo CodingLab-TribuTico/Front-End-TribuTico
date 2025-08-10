2d1e091f8023936bee733868b070c02f8f806522
diegonunez19 <tu_correo_de_diegonunez19@gmail.com>
2025-08-10 15:42:11 -0600

    add search functionality by type

--- src/app/pages/simulation-view/simulation-view.component.ts
+++ src/app/pages/simulation-view/simulation-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject } from "@angular/core";
+import { Component, computed, inject, signal } from "@angular/core";
 import { SimulationListComponent } from "../../components/simulation-list/simulation-list.component";
 import { PaginationComponent } from "../../components/pagination/pagination.component";
 import { LoaderComponent } from "../../components/loader/loader.component";
@@ -25,22 +25,30 @@ export class SimulationViewComponent {
   public isrExportService = inject(IsrExportService);
   public ivaExportService = inject(IvaExportService);
   public alertService = inject(AlertService);
-  
-  public allSimulations = computed(() => {
-    const iva = this.ivaService.simulationsIva$().map(ivaCalculation => (
-      { ...ivaCalculation, type: 'IVA' }));
+  public searchType = signal<string>(''); 
 
-    const isr = this.isrService.simulationsIsr$().map(isrSimulation => (
-      { ...isrSimulation, type: 'ISR' }));
-    return [...iva, ...isr];
+  public allSimulations = computed(() => {
+    const iva = this.ivaService.simulationsIva$().map(i => ({ ...i, type: 'IVA' }));
+    const isr = this.isrService.simulationsIsr$().map(s => ({ ...s, type: 'ISR' }));
+    const all = [...iva, ...isr];
+
+    const search = this.searchType().trim();
+    if (!search) return all;
+
+    return all.filter(sim => sim.type.toLowerCase().includes(search));
   });
 
+  search(event: Event) {
+    const value = (event.target as HTMLInputElement).value || '';
+    this.searchType.set(value.trim().toLowerCase());
+  }
+  
   constructor() {
     const user = JSON.parse(localStorage.getItem('auth_user') || '{}');
     const userId = user.id;
     this.loadSimulations(userId);
   }
-
+  
   loadSimulations(userId: number) {
     this.ivaService.getByUserId(userId);
     this.isrService.getByUserId(userId);
@@ -72,21 +80,6 @@ export class SimulationViewComponent {
 
   isIvaSimulation(simulation: any): boolean {
     return simulation && typeof simulation.year === 'number' && typeof simulation.month === 'number';
-  }
-
-  search(event: Event) {
-  const input = (event.target as HTMLInputElement).value
-    .trim()
-    .toLowerCase();
-
-  this.isrService.search.page = 1;
-  this.ivaService.search.page = 1;
-
-  this.isrService.search.search = input;
-  this.ivaService.search.search = input;
-
-  this.isrService.getAll();
-  this.ivaService.getAll();
   }
 
 }
